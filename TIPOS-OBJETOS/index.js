69d1b648bab92e5c2b1da3ee59bf133a46df6fe1
Giselle jacinto <gisellejacinto1219@gmail.com>
2023-11-14 17:00:50 -0300

    tipos de objetos JS
    joyclass tipos de objetos

---
+++ TIPOS-OBJETOS/index.js
@@ -0,0 +1,49 @@
+
+/*
+OBJETOS MATERIAIS
+1-
+Material do Objeto: Carro
+Atributo
+Modelo: O modelo específico do carro.
+Cor: A cor do carro.
+Ano de Fabricação:O ano em que o carro foi fabricado.
+Velocidade Atual: A velocidade atual do carro em km/h.
+Ligado: Indica se o
+Métodos:
+ligar(): Liga o carro.
+desligar(): Des
+acelerar(velocidade): Acelera o
+frear(): Freia o carro
+obterInformações(): retorna informações sobre o carro.
+
+2- Notebook
+Atributos 
+Marca, Modelo e Processador.
+Métodos 
+Ligar/desligar, executar programa, expandir memória, Armazenamento, obter informações.
+
+
+
+
+OBJETOS ABSTRATOS
+
+1 -
+Atributos
+Nome da Rede: O nome da rede social.
+Usuários: Uma lista de usuários
+Publicações: As postagens feitas na rede social
+Métodos:
+AdicionarUsuário: Adiciona um novo usuário à rede.
+CriarPublicacao: Cria uma nova publicação na rede
+ListarPublicações: Lista as publicações
+
+2-
+Atributos:
+Número da Conta: O número da conta
+Saldo: O saldo atual
+Titular: O titular da conta
+Métodos:
+Depositário: Real
+Sacar: Realiza
+ConsultarSaldo: Retorno do saldo atual da contaS
+*/
